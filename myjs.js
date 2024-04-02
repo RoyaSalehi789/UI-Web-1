0914b1448dcac2b3db4f4fa61bc3b9103d363c5d
roya <roya.salehi789@gmail.com>
2024-04-02 14:12:24 +0330

    login-modal added using js

--- myjs.js
+++ myjs.js
@@ -1,4 +1,4 @@
-function openCity(evt, cityName) {
+function openCity(evt, tabName) {
     // Declare all variables
     var i, tabcontent, tablinks;
   
@@ -18,3 +18,24 @@ function openCity(evt, cityName) {
     document.getElementById(cityName).style.display = "block";
     evt.currentTarget.className += " active";
   }
+
+  // log-in
+
+var modal = document.getElementById("log-in-modal");
+var btn = document.getElementById("log-in-btn");
+
+var span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
