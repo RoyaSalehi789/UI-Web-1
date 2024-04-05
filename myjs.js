71de326c191c8ec97a7d1f76208f58ba599c518a
roya <roya.salehi789@gmail.com>
2024-04-05 14:54:37 +0330

    modals updated and some hover added

--- myjs.js
+++ myjs.js
@@ -13,16 +13,42 @@ function switchTab(evt, tabName) {
   }
 
 // log-in
-function modals(modal, btn){
+function modals(modal, button){
   var modal = document.getElementById(modal);
-  var btn = document.getElementById(btn);
+  var btn = document.getElementById(button);
 
-  var span = document.getElementsByClassName("close")[0];
+  var loginclose = document.getElementsByClassName("close")[0];
+  var registerclose = document.getElementsByClassName("close")[1];
+  var otpclose = document.getElementsByClassName("close")[2];
+
 
   btn.onclick = function() {
-    modal.style.display = "block";
+    if(button == "log-in-btn") {
+      modal.style.display = "block";
+    }
+    if(button == "register-btn") {
+      document.getElementById("log-in-modal").style.display = "none";
+      modal.style.display = "block";
+    }
+    if(button == "otp-btn") {
+      document.getElementById("register-modal").style.display = "none";
+      modal.style.display = "block";
+    }
+    if(button == "Successfull-btn") {
+      document.getElementById("otp-modal").style.display = "none";
+      modal.style.display = "block";
+    }
+    if(button == "cart-btn") {
+      modal.style.display = "block";
+    }
   }
-  span.onclick = function() {
+  loginclose.onclick = function() {
+    modal.style.display = "none";
+  }
+  registerclose.onclick = function() {
+    modal.style.display = "none";
+  }
+  otpclose.onclick = function() {
     modal.style.display = "none";
   }
 
