a9e95ffa7e3e903ae4ed7e7148eed12b898b5d1f
roya <roya.salehi789@gmail.com>
2024-04-17 12:30:07 +0330

    add/remove item cart js and rating js added

--- myjs.js
+++ myjs.js
@@ -114,42 +114,6 @@ function changePriceByColor(button) {
   }
 }
 
-//cart-class
-class Product {
-  constructor(name, rate, reviewsCont, price, color, description, image) {
-    this.name = name;
-    this.rate = rate;
-    this.reviewsCont = reviewsCont;
-    this.price = price;
-    this.color = color;
-    this.description = description;
-    this.image = image;
-  }
-}
-//--iphone-14-pro--
-const product1 = new Product(
-  "Apple iPhone 14 Pro",
-   5.0, 
-   121,
-   1999.00, 
-   "#BEBEC6", 
-"It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution.", 
-"images/image 16 (1).png"
-);
-const product2 = new Product(
-  "Asus ROG Delta S",
-   5.0, 
-   121,
-   250.00, 
-   "#BEBEC6", 
-"It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution.", 
-"images/image 16 (1).png"
-);
-
-var cartList = [];
-cartList.push(product1);
-cartList.push(product2);
-
 //product-count-cart
 function plusMinus(button) {
   var cont = parseInt(document.getElementById("product-counter").innerHTML);
@@ -165,25 +129,43 @@ function plusMinus(button) {
 
 //add-to-cart
 function addToCart() {
-  var productName = document.getElementById("product-name");
-  var productprice = document.getElementById("product-price");
-  var productNum = document.getElementById("product-count");
-  const iphoneNum = document.getElementById("item1");
-  const rogNum = document.getElementById("item2");
-
-  var newProduct = new Product(productName, 0, 0, productprice, "", "", "images/image 16 (1).png");
-
-  for (let i = 0 ; i < cartList.length ; i++) {
-    if(JSON.stringify(cartList[i]) === JSON.stringify(newProduct)) {
-      
-
-
-    }
-    else {
-      cartList.push(newProduct);
-    }
-  }
+  var iphone = document.getElementsByClassName("price-item-cart")[0];
+  var iphoneCount = parseInt(iphone.innerHTML.substring(5));
+  var itemCount = parseInt(document.getElementById("product-counter").innerHTML);
+  var icon = document.getElementById("cart-num");
+  var cartNumber = parseInt(icon.innerHTML);
+  icon.innerHTML = `${cartNumber + itemCount}`;
+  iphone.innerHTML = `QTY: ${iphoneCount + itemCount}`;
 }
 
+// delete-item-cart
+function deleteItemCart(index) {
+  /*remove product*/
+  var item = document.getElementsByClassName("delete")[index].parentElement.parentElement.parentElement;
+  item.style.display = "none";
+  item.nextElementSibling.style.display = "none";
+  /*updating cart icon*/
+  var product = document.getElementsByClassName("price-item-cart")[index];
+  var productCount = parseInt(product.innerHTML.substring(5));
+  var icon = document.getElementById("cart-num");
+  var cartNumber = parseInt(icon.innerHTML);
+  icon.innerHTML = `${cartNumber - productCount}`;
+  /*updating cart text*/
+  var numberText = parseInt(document.getElementById("item-cart-count-text").innerHTML.substring(0, 1));
+  document.getElementById("item-cart-count-text").innerHTML = `${numberText - 1} items`;
+}
 
+//scoring
+function scoring(index) {
+  console.log("Your Rating: " + `${index + 1}`);
+  var starBox = document.getElementsByClassName("star")[index];
+  var stars = starBox.getElementsByTagName("img");
+  var allStars = document.getElementsByClassName("rating")[0].getElementsByTagName("img");
 
+  for (var j = 0 ; j < allStars.length ; j++) {
+    allStars[j].src = "icons/star.png";
+  }
+  for (var j = 0 ; j < stars.length ; j++) {
+    stars[j].src = 'icons/Star 2.png';
+  }
+}
